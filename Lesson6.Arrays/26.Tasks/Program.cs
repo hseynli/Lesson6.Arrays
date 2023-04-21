fb18c88587ede06cd6825a8daf24939d0653a8b0
Farid <hseynli@list.ru>
2023-04-21 20:12:21 +0400

    Added Tasks to Lesson

---
+++ Lesson6.Arrays/26.Tasks/Program.cs
@@ -0,0 +1,9 @@
+﻿// 10 ölçülü massivdə ək böyük elementi tapın
+// 10 ölçülü massivdə ən kiçik elementi tapın
+// daxilində 5 massivdən ibarət jagged massiv yaradın. Həmin jagged massivin içindən ən çox elementi olan massivi tapın
+// 10 elementdən ibarət string array yaradın və onun elementlərini ixtayir insan adı ilə doldurun. Həmin adlar içərisində ən uzun olan adı ekranda əks elətdirin. string.Length
+// Metod yaradın. Metod params int arqument qəbul etsin. Geriyə params arqumentlərinin cəmin qaytarılsın.
+// 10 elementdən ibarət massiv verilir. Elementlərin bəzilərini mənfi edin. Mənfi olan elementləri ekranda əks elətdirin.
+// 10 elementdən ibarət massiv verilir. Elementlərin bəzilərini mənfi edin. Mənfi olan elementləri yeni massivə mənimsədin. CopyTo metodundan istifadə edilə bilər
+// 10 ölçülü massiv yaradın. Massivin elementlərini istifadə daxil etsin. Daha sonra nəticəni ekranda əks elətdirin
+//
